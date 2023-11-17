b3b84dc6d9d22d41fbc3db56fda49835acd3952b
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 12:49:27 -0300

    change headers

--- client/src/components/DonationForm/DonationForm.jsx
+++ client/src/components/DonationForm/DonationForm.jsx
@@ -28,34 +28,9 @@ const DonationForm = () => {
 
     
     //MERCADO PAGO FUNCTIONS
-    // const createPreference = async () => {
-    //     const headers = {
-    //         'Access-Control-Allow-Origin': ['*'],
-    //         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-    //         'Access-Control-Allow-Headers': 'Content-Type'
-    //       }
-    //     try {
-            
-    //         const response = await axios.post('https://patitas-callejeras-back-oymdo79hy-tomaslaus.vercel.app/create_preference', headers,
-    //         {
-    //             description: 'gracias por los cafecitos',
-    //             price: totalAmount,
-    //             quantity: 1
-    //         });
 
-    //         const { id } = response.data;
-    //         return id;
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
 
     const createPreference = async () => {
-        // const headers = {
-        //   'Access-Control-Allow-Origin': 'https://patitas-callejeras-back.vercel.app/',
-        //   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-        //   'Access-Control-Allow-Headers': 'Content-Type'
-        // };
         try {
           const response = await fetch('https://patitas-callejeras-back.vercel.app/create_preference', {
             method: 'POST',
