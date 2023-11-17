ca9cc19d9e1099cf0ebbaca4f269d102114452f9
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 14:15:15 -0300

    change vite.cfg

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-    '/server': {
-      target: 'https://patitas-callejeras-back.vercel.app',
-      changeOrigin: true,
-      secure: false,
-      rewrite: (path) => path.replace(/^\/server/, '')
-    },
-    cors:false
-    },
-  },
 })
