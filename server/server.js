f4006aa9304834ee6f2ce494f9b77322a6bf0a32
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 12:18:53 -0300

    change headers server

--- server/server.js
+++ server/server.js
@@ -15,9 +15,10 @@ mercadopago.configure({
 
 
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });  
 
 
