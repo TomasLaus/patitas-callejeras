09f83bf5b5cc395f46d2c584f74ae96dc69b713d
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 12:07:44 -0300

    change url donationform

--- client/src/components/DonationForm/DonationForm.jsx
+++ client/src/components/DonationForm/DonationForm.jsx
@@ -34,7 +34,7 @@ const DonationForm = () => {
         };
         try {
             
-            const response = await axios.post('https://patitas-callejeras-back.vercel.app/create_preference', {headers},
+            const response = await axios.post('https://patitas-callejeras-back-cxcmsby8y-tomaslaus.vercel.app/create_preference', {headers},
             {
                 description: 'gracias por los cafecitos',
                 price: totalAmount,
