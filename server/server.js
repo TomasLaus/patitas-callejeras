fbe127bed5b4883ca09e4249574dfe91604bfe41
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 14:02:31 -0300

    change headers

--- server/server.js
+++ server/server.js
@@ -15,7 +15,11 @@ mercadopago.configure({
 
 
 app.use(cors());
-
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 
 
