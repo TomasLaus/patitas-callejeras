40be6cfd579da57fc0c07559cab1f72b1243c345
Tomás Laus <83286315+TomasLaus@users.noreply.github.com>
2023-11-17 11:57:20 -0300

    change url front vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+    '/api': {
+      target: 'https://patitas-callejeras-back.vercel.app',
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    },
+    cors:false
+    },
+  },
 })
